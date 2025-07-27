2d07fb1c25d18459f1f63019e862d0b043d7ad54
Buddha Dev Sarker <bsarker@miu.edu>
2025-07-27 18:16:16 -0400

    Basic Identity

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,9 +1,21 @@
+using AuthAPI.Models;
+using CouponAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
