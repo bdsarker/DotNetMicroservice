17003a42229486c87ff8539661151c86f2c71641
Buddha Dev Sarker <bsarker@miu.edu>
2025-07-29 20:30:18 -0400

    Show Homepage Producta

--- WebApp/Models/ProductDto.cs
+++ WebApp/Models/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class ProductDto
     {
@@ -8,5 +10,7 @@
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
     }
 }
